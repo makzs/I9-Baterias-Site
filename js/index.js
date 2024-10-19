4472f3d36524985858c2ef26cf6d131660b72e72
Fernanda Piekas <fepiekas@gmail.com>
2024-10-19 19:52:35 -0300

    slides na pagina inicial e alteracao de fundo

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// javascript
+    let counter = 1;
+    setInterval(function(){
+        document.getElementById('radio' + counter).checked = true;
+        counter++;
+        if(counter > 3){
+            counter = 1;
+        }
+    }, 5000); // Altera o slide a cada 5 segundos
+
