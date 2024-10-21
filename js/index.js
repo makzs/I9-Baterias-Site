5967930a7a57bb2cc1eba7e595b82942f8f76db4
makzs <erikmakiak@gmail.com>
2024-10-21 17:52:27 -0300

    implementando bootstrap

--- js/index.js
+++ js/index.js
@@ -1,9 +1,1 @@
-    let counter = 1;
-    setInterval(function(){
-        document.getElementById('radio' + counter).checked = true;
-        counter++;
-        if(counter > 3){
-            counter = 1;
-        }
-    }, 5000); // Altera o slide a cada 5 segundos
-
+// javascript
